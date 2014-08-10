4eab174b13e1dd1457c8317d597b2b670de4f4b9
Fredrik Ohlin <fohlin@gmail.com>
2014-08-10 21:51:02 +0200

    Added special page for weekends

--- dockanlunch/__init__.py
+++ dockanlunch/__init__.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+
+from datetime import datetime
 from flask import Flask, render_template, url_for
 from werkzeug.contrib.cache import SimpleCache
 from models import get_all
@@ -9,5 +12,8 @@ cache = SimpleCache()
 @app.route("/")
 def index():
     r = get_all(cache)
-    return render_template("index.html", restaurants=r)
+    if not datetime.now().weekday() in (5,6):
+        return render_template("index.html", restaurants=r)
+    else:
+        return render_template("weekend.html", restaurants=r)
 
